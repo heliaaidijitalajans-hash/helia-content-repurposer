c63b3e68ed0b343717b7310746f9d7555a182a8f
Fevzi <helyora349@gmail.com>
2026-04-07 23:11:46 +0300

    tüm proje

--- app/api/transcribe/route.ts
+++ app/api/transcribe/route.ts
@@ -2,8 +2,8 @@ import { NextResponse } from "next/server"
 import { createClient } from "@supabase/supabase-js"
 
 const supabase = createClient(
-  process.env.NEXT_SUPABASE_URL!,
-  process.env.NEXT_SUPABASE_SERVICE_ROLE_KEY!
+  process.env.SUPABASE_URL!,
+  process.env.SUPABASE_SERVICE_ROLE_KEY!
 )
 
 export async function POST(req: Request) {
